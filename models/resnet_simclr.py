53a3e1a2f521a85e3a95700f5a2a070e1fc63a9b
m-kuoch <michael.kuoch@dexai.com>
2020-07-05 16:14:09 -0400

    Added simpler model definition.  Changed simclr.py to use this model.

--- models/resnet_simclr.py
+++ models/resnet_simclr.py
@@ -2,6 +2,42 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 
+class SimpleSimCLR(nn.Modlue):
+    class ResNetSimCLR(nn.Module):
+
+        def __init__(self, encode_size, out_dim):
+            super(SimpleSimCLR, self).__init__()
+
+            self.conv1 = nn.Conv2d(1, 32, 3, 1)
+            self.relu1 = nn.ReLU()
+            self.conv2 = nn.Conv2d(32, 64, 3, 1)
+            self.relu2 = nn.ReLU()
+            self.max_pool = nn.MaxPool2d(2)
+            self.flatten = nn.Flatten()
+            self.fc1 = nn.Linear(9216, encode_size)
+            self.relu3 = nn.ReLU()
+
+            # projection MLP
+
+            self.l1 = nn.Linear(encode_size, encode_size)
+            self.l2 = nn.Linear(encode_size, out_dim)
+
+
+        def forward(self, x):
+            x = self.conv1(x)
+            x = self.relu1(x)
+            x = self.conv2(x)
+            x = self.relu2(x)
+            x = self.max_pool(x)
+            x = self.flatten(x)
+            x = self.fc1(x)
+            h = self.relu3(x)
+            # Projection before loss
+            x = self.l1(h)
+            x = F.relu(x)
+            x = self.l2(x)
+
+            return h, x
 
 class ResNetSimCLR(nn.Module):
 
